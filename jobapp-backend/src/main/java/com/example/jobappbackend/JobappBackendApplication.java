a2521fa9c4d6b69e41021d8d0205e4bc10067911
kossila227754 <127935565+kossila227754@users.noreply.github.com>
2025-07-28 21:19:06 +0200

    Initial commit: frontend and backend

---
+++ jobapp-backend/src/main/java/com/example/jobappbackend/JobappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobappBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobappBackendApplication.class, args);
+    }
+
+}
---
+++ jobapp-backend/src/test/java/com/example/jobappbackend/JobappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
