35ff58050d4855cee4bf09ffcf21db0ca531ac8e
kossila227754 <127935565+kossila227754@users.noreply.github.com>
2025-08-14 19:30:53 +0200

    create user dialog to update user info

--- jobapp-frontend/src/app/core/components/admin/pages/dialogs/user-dialog/user-dialog.ts
+++ jobapp-frontend/src/app/core/components/admin/pages/dialogs/user-dialog/user-dialog.ts
@@ -1,65 +1,61 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../../../services/auth.service';
+
 
 export type Role = 'company' | 'student' | 'admin';
 
 export interface User {
   id: number;
+  username: string;
   firstName: string;
   lastName: string;
   email: string;
-  createdAt: Date;
   role: Role;
-  password?: string;
+  companyName?: string;
+  address?: string;
+  phoneNumber?: string;
 }
 
 @Component({
   selector: 'app-user-dialog',
-  standalone:false,
+  standalone: false,
   templateUrl: './user-dialog.html',
-  styleUrls: ['./user-dialog.scss']
+  styleUrls: ['./user-dialog.css']
 })
 export class UserDialog implements OnInit {
   userForm: FormGroup;
+  currentAdminUsername?: string;
 
   constructor(
     public dialogRef: MatDialogRef<UserDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: { user: User | null, isEdit: boolean },
-    private fb: FormBuilder
+    @Inject(MAT_DIALOG_DATA) public data: { user: User; isEdit: boolean },
+    private fb: FormBuilder,
+    private authService: AuthService
   ) {
+    const payload = this.authService.decodePayload();
+    this.currentAdminUsername = payload?.username;
     this.userForm = this.createForm();
   }
 
   ngOnInit(): void {
     if (this.data.user && this.data.isEdit) {
-      this.userForm.patchValue({
-        firstName: this.data.user.firstName,
-        lastName: this.data.user.lastName,
-        email: this.data.user.email,
-        role: this.data.user.role
-      });
+      this.userForm.patchValue(this.data.user);
     }
   }
 
   createForm(): FormGroup {
-    if (this.data.isEdit) {
-      return this.fb.group({
-        firstName: ['', Validators.required],
-        lastName: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', Validators.minLength(8)],
-        role: ['', Validators.required]
-      });
-    } else {
-      return this.fb.group({
-        firstName: ['', Validators.required],
-        lastName: ['', Validators.required],
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(8)]],
-        role: ['', Validators.required]
-      });
-    }
+    return this.fb.group({
+      username: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      role: ['', Validators.required],
+      companyName: [''],
+      address: [''],
+      phoneNumber: ['']
+    });
   }
 
   onCancel(): void {
@@ -68,11 +64,7 @@ export class UserDialog implements OnInit {
 
   onSave(): void {
     if (this.userForm.valid) {
-      const formValue = { ...this.userForm.value };
-      if (this.data.isEdit && !formValue.password) {
-        delete formValue.password;
-      }
-      this.dialogRef.close(formValue);
+      this.dialogRef.close(this.userForm.value);
     }
   }
 }
